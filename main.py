16fc6352b2fa8508936dbf5ba7a8380f0281672a
Thomas_dev <michael.fomin.89@gmail.com>
2020-03-16 21:30:48 +0300

    click counter json database

--- main.py
+++ main.py
@@ -11,9 +11,21 @@ window.geometry("300x300+500+300")
 
 my_lable = Label(window, bg="red")
 
-my_lable["text"] = "Hello!!!"
+my_lable["text"] = "0"
 
-#my_lable.pack(side="left")
-my_lable.place({'x': 50, 'y': 50, "width":50, "height": 50})
+button = Button()
 
+button["text"] = "click"
+
+button.place({'x': 0, 'y': 0, "width": 100, "height": 30})
+
+my_lable.place({'x': 125, 'y': 125, "width": 50, "height": 50})
+
+
+def click():
+    current_number = int(my_lable["text"])
+    my_lable["text"] = str(current_number + 1)
+
+
+button["command"] = click
 window.mainloop()
